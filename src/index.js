efd71162c161cc031091baf21d2147fca5f7caa5
yabao0x5a <yabao.0x5a@gmail.com>
2023-10-01 10:22:34 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,12 +7,19 @@ const { Pool } = require("pg");
 const app = express();
 const port = process.env.PORT || 5000;
 
+const dbUri =
+  "postgres://hqugikqrtjkegi:cb8858680683862a5cd3ed175cdcb02d6a3ee7e4cf25230590d708d2fc2821cc@ec2-52-45-200-167.compute-1.amazonaws.com:5432/d2mrhpp8mj639a";
+
+// const pool = new Pool({
+//   user: "hqugikqrtjkegi",
+//   host: "ec2-52-45-200-167.compute-1.amazonaws.com",
+//   database: "d2mrhpp8mj639a",
+//   password: "cb8858680683862a5cd3ed175cdcb02d6a3ee7e4cf25230590d708d2fc2821cc",
+//   port: 5432, // Default PostgreSQL port
+// });
+// PostgreSQL database configuration
 const pool = new Pool({
-  user: "hqugikqrtjkegi",
-  host: "ec2-52-45-200-167.compute-1.amazonaws.com",
-  database: "d2mrhpp8mj639a",
-  password: "cb8858680683862a5cd3ed175cdcb02d6a3ee7e4cf25230590d708d2fc2821cc",
-  port: 5432, // Default PostgreSQL port
+  connectionString: dbUri,
 });
 
 app.use(cors());
